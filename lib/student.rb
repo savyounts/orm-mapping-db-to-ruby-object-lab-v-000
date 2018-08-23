8fc19fb7d4d79557e7686b84c207ce793735754e
Sav Younts <savyounts@gmail.com>
2018-08-23 15:56:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,18 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10 (x)
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE students.grade = 10
+    LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students
