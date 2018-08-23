6261d36b47a10f63efe552cf74e4b6c397d98416
Sav Younts <savyounts@gmail.com>
2018-08-23 15:50:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,15 @@ class Student
       end
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL 
+    SELECT * FROM students
+    WHERE students.grade = 9
+    SQL 
+
+    DB[:conn].execute(sql)
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
     SELECT * FROM students
